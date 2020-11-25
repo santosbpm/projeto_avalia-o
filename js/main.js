0107ddd2f839673847225a5abf6563c6b01591a9
santosbpm <santosbpm.0@gmail.com>
2020-11-25 03:04:49 -0300

    Finalizando estrutura do site

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function redirect_information() {
+    alert("Você será redirecionado para um site de teste de personalidade. \n" + 
+    "A tipologia de Myers-Briggs é um instrumento utilizado para identificar \n" +
+    "características (incluindo introversão e extroversão) e preferências pessoais");
+}
